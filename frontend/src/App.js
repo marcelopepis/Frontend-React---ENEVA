b5abf5729b90de79773fd79f97438da84523efee
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-03-09 16:49:50 -0300

    Separando o css da pagina de login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import logo from './assets/atos-logo.png'
 import './services/api';
 import Routes from './routes';
+
 
 function App() {
   
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar'
+import '../Dashboard/index.css'
 
 export default function Dashboard() {
   return (
