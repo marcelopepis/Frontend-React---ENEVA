a56cc6f9def650239fee3dcf2bb9a62e06e884a1
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-03-15 16:30:22 -0300

    Update subbacias.js

--- frontend/src/pages/subbacias/subbacias.js
+++ frontend/src/pages/subbacias/subbacias.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar'
 import "./subbacias.css";
 
 export default function Subbacias() {
+  const [ numSubbacia, setNumSubbacia ] = useState('');
+
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("teste");
+  }
+
+
+
   return (
     <>
       <Sidebar></Sidebar>
-      <h1>subbacias</h1>
+      <div className="container">
+        <div className="content">
+          <p>Cadastro de <strong>Subbacias</strong></p>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="numSubbacia">Número da Subbacia</label>
+            <input type="text"
+                    id="numSubbacia"
+                    placeholder="subbacia"
+                    value={numSubbacia}
+                    onChange = {event => setNumSubbacia(event.target.value)}>
+
+            </input>
+
+
+          </form>
+        </div>
+      </div>
     </>
   )
 }
