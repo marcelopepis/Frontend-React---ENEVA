175c80202ab12d28179d19bc077b300d542e27d0
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-03-15 16:55:32 -0300

    Update subbacias.js
    Finalizada a tela de cadastro de subbacia.

--- frontend/src/pages/subbacias/subbacias.js
+++ frontend/src/pages/subbacias/subbacias.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar'
 import "./subbacias.css";
 
-export default function Subbacias() {
+export default function Subbacias({ history }) {
   const [ numSubbacia, setNumSubbacia ] = useState('');
+  const [ nome, setNome ] = useState('');
+  const [ idBacia, setIdBacia ] = useState('');
 
 
   async function handleSubmit(event) {
     event.preventDefault();
     console.log("teste");
+    history.push('/dashboard');
+  }
+  function handleCancel() {
+    console.log("cancelado subbacia");
+    history.push('/dashboard');
   }
 
 
@@ -23,13 +30,32 @@ export default function Subbacias() {
             <label htmlFor="numSubbacia">Número da Subbacia</label>
             <input type="text"
                     id="numSubbacia"
-                    placeholder="subbacia"
+                    placeholder="Subbacia"
+                    required
                     value={numSubbacia}
                     onChange = {event => setNumSubbacia(event.target.value)}>
-
             </input>
-
-
+            <label htmlFor="nome">Nome</label>
+            <input type="text"
+                    id="nome"
+                    placeholder="Nome da Subbacia"
+                    value = {nome}
+                    required
+                    onChange = {event => setNome(event.target.value)}>
+            </input>
+            <label>
+              Escolha a Bacia
+              <select value={idBacia} required onChange={event => setIdBacia(event.target.value)}>
+                <option value="01">01 Bacia Teste</option>
+                <option value="02">02 Bacia Teste</option>
+                <option value="03">03 Bacia Teste</option>
+                <option value="04">04 Bacia Teste</option>
+              </select>
+            </label>
+            <div className="btnArea">
+              <button className = "btnCancelar" onClick={handleCancel}>Cancelar</button>
+              <button className = "btnSalvar" type = "submit">Salvar</button>
+            </div>
           </form>
         </div>
       </div>
