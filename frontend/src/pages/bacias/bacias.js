7d1f1b2267790346986178dbb71572bac17f6845
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-03-16 14:11:32 -0300

    adaptado um modal para mensagens de sucesso ou falha na inserção de dados.

--- frontend/src/pages/bacias/bacias.js
+++ frontend/src/pages/bacias/bacias.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar'
 import "./bacias.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 //import api from '../../services/api';
 
 
@@ -15,13 +19,52 @@ export default function Bacias({ history }) {
     console.log(numBacia);
     console.log(nome);
     console.log(idRegiao);
-
-    history.push('/dashboard');
+    handleOpen();
+    //history.push('/dashboard');
   };
 
-  function handleCancel(event){
-    console.log("cancelado bacias");
+  const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #ddd',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  function handleCancel() {
+    
+    console.log("cancelado subbacia");
+    //handleOpen();
     history.push('/dashboard');
+  }
+
+  function handleFinishModal(){
+    history.push('/dashboard');
+  }
+
+  function handleNewModal(){
+    
+    handleClose();
+    history.push('/bacias');
+  }
+
+  //modal de aviso ao usuário do status do cadastro.
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -61,10 +104,35 @@ export default function Bacias({ history }) {
               <button className = "btnCancelar" onClick={handleCancel}>Cancelar</button>
               <button className = "btnSalvar" type = "submit">Salvar</button>
             </div>
-            
-
           </form>
         </div>
+      </div>
+
+      <div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">Bacia <strong>cadastrada</strong></h2>
+              <p id="transition-modal-description">Informações cadastradas com sucesso!</p>
+              <div className="btnAreaModal">
+                <button className = "btnCancelarModal" onClick={handleFinishModal}>Painel</button>
+                <button className = "btnSalvarModal" onClick={handleNewModal}>Novo</button>
+              </div>
+            </div>
+            
+          </Fade>
+        </Modal>
       </div>
     </>
   )
--- frontend/src/pages/subbacias/subbacias.js
+++ frontend/src/pages/subbacias/subbacias.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar'
 import "./subbacias.css";
 //import api from '../../services/api';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 export default function Subbacias({ history }) {
   const [ numSubbacia, setNumSubbacia ] = useState('');
@@ -9,15 +13,60 @@ export default function Subbacias({ history }) {
   const [ idBacia, setIdBacia ] = useState('');
 
 
+
   async function handleSubmit(event) {
     event.preventDefault();
+    console.log(numSubbacia);
+    console.log(nome);
     console.log("teste");
-    history.push('/dashboard');
+    handleOpen();
   }
+
+  const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #ddd',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
   function handleCancel() {
+    
     console.log("cancelado subbacia");
+    //handleOpen();
     history.push('/dashboard');
   }
+
+  function handleFinishModal(){
+    history.push('/dashboard');
+  }
+
+  function handleNewModal(){
+    
+    handleClose();
+    history.push('/subbacias');
+  }
+
+  //modal de aviso ao usuário do status do cadastro.
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
 
   return (
     <>
@@ -53,11 +102,41 @@ export default function Subbacias({ history }) {
             </label>
             <div className="btnArea">
               <button className = "btnCancelar" onClick={handleCancel}>Cancelar</button>
-              <button className = "btnSalvar" type = "submit">Salvar</button>
+              <button className = "btnSalvar" type="submit">Salvar</button>
             </div>
           </form>
         </div>
       </div>
+
+      <div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">Subbacia cadastrada</h2>
+              <p id="transition-modal-description">Informações cadastradas com sucesso!</p>
+              <div className="btnAreaModal">
+                <button className = "btnCancelarModal" onClick={handleFinishModal}>Painel</button>
+                <button className = "btnSalvarModal" onClick={handleNewModal}>Novo</button>
+              </div>
+            </div>
+            
+          </Fade>
+        </Modal>
+      </div>
+
+
+
     </>
   )
 }
